e29d1e66edc5cc65e854392a0da2230c0f532ca1
captainlomik <nastiy248@mail.ru>
2024-06-08 00:04:03 +0300

    add log screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Input } from './shared/input';
+import { Button } from './shared/button';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Gaps } from './shared/tokens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on yjjjour app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text>School </Text>
+        <Image style={styles.logo}
+        source={require('./assets/logo.png')}
+        resizeMode='contain' />
+        <View style={styles.form}>
+          <Input placeholder='Email'/>
+          <Input isPassword placeholder='Пароль'/>
+          <Button text='Войти'/>
+        </View>
+        <Text> Восстановить пароль </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 55,
+    backgroundColor: Colors.black,
   },
+  content: {
+    alignItems: 'center',
+    gap: Gaps.g50,
+  },
+  form: {
+    alignSelf: 'stretch',
+    gap: Gaps.g16,
+  },
+  logo:{
+
+  }
 });
