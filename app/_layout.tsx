b9258402e7c22ec484f56ce959207bef46b34c92
captainlomik <nastiy248@mail.ru>
2024-06-08 22:15:45 +0300

    add splashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,34 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { Colors } from "../shared/tokens";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from 'expo-font';
 import { SafeAreaProvider, useSafeAreaFrame, useSafeAreaInsets } from "react-native-safe-area-context";
+import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
     const insets = useSafeAreaInsets();
+    const [loaded, error] = useFonts({
+        FiraSans: require('../assets/font/FiraSans-Regular.ttf'),
+        FiraSansSemiBold: require('../assets/fonts/FiraSans-SemiBold.ttf')
+    })
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync()
+        }
+    }, [loaded])
+
+    useEffect(() => {
+        if (error) {
+            throw error
+        }
+    }, [error])
+
+    if (!loaded) {
+        return null;
+    }
 
     return (
         <SafeAreaProvider>
