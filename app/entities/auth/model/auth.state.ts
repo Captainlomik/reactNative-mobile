1a1ad18a6d01198d2eb44b24bd1fd8aac5120224
captainlomik <nastiy248@mail.ru>
2024-06-10 01:00:03 +0300

    work with auth and stage

---
+++ app/entities/auth/model/auth.state.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
+
+const storage = createJSONStorage<AuthState>(() => AsyncStorage)
+
+export const authAtom = atomWithStorage<AuthState>('auth', {
+    access_token: null, 
+    isLoadind: false, 
+    error: null
+}, storage)
+
+export interface AuthState{
+    access_token: string | null, 
+    isLoadind: boolean, 
+    error: string | null,
+}
+
