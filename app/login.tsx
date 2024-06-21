94b7eae221bfea64bf799603e7776c85730c9360
Sergei Pshonnov <43892086+kkeigenkai@users.noreply.github.com>
2024-06-21 21:51:46 +0300

    upd

--- app/login.tsx
+++ app/login.tsx
@@ -1,48 +1,54 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View, Image, Dimensions,KeyboardAvoidingView,Platform} from 'react-native';
-import { Input } from '../shared/input';
-import { Button } from '../shared/button';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { Gaps } from '../shared/tokens';
-import { ErrorNotification } from '../shared/ErrorNotification/errorNotification';
-import { useEffect, useState } from 'react';
-import { CustomLink } from '../shared/CustomLink';
-import { loginAtom } from './entities/auth/model/auth.state';
-import { useAtom } from 'jotai';
-import { router } from 'expo-router';
-import { useScreenOrientation } from '../shared/hooks';
-import { Orientation } from 'expo-screen-orientation';
-import { validationEmail } from '../shared/validation';
-
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	Dimensions,
+	KeyboardAvoidingView,
+	Platform,
+} from "react-native";
+import { Input } from "../shared/input";
+import { Button } from "../shared/button";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Gaps } from "../shared/tokens";
+import { ErrorNotification } from "../shared/ErrorNotification/errorNotification";
+import { useEffect, useState } from "react";
+import { CustomLink } from "../shared/CustomLink";
+import { loginAtom } from "./entities/auth/model/auth.state";
+import { useAtom } from "jotai";
+import { router } from "expo-router";
+import { useScreenOrientation } from "../shared/hooks";
+import { Orientation } from "expo-screen-orientation";
+import { validationEmail } from "../shared/validation";
 
 export default function Login() {
 	const [localErorr, setLocalError] = useState<string | undefined>();
 	const [email, setEmail] = useState<string>();
 	const [password, setPassword] = useState<string>();
-	const [{ access_token, isLoading}, login] = useAtom(loginAtom);
+	const [{ access_token, isLoading }, login] = useAtom(loginAtom);
 	const orientation = useScreenOrientation();
 
 	const submit = () => {
 		if (!email) {
-			setLocalError('Не введен email');
+			setLocalError("Не введен email");
 			return;
 		}
-		if (validationEmail(email)) {
-			setLocalError('Email не соответсвует формату email@mail.ru');
+		if (!validationEmail(email)) {
+			setLocalError("Email не соответсвует формату email@mail.ru");
 			return;
 		}
 		if (!password) {
-			setLocalError('Не введен пароль');
+			setLocalError("Не введен пароль");
 			return;
 		}
 		if (password.length < 6) {
-			setLocalError('Пароль не может быть менее 6 символов');
+			setLocalError("Пароль не может быть менее 6 символов");
 			return;
 		}
 		login({ email, password });
 	};
-
 
 	useEffect(() => {
 		if (localErorr) {
@@ -52,7 +58,7 @@ export default function Login() {
 
 	useEffect(() => {
 		if (access_token) {
-			router.replace('/(app)');
+			router.replace("/(app)");
 		}
 	}, [access_token]);
 
@@ -60,24 +66,29 @@ export default function Login() {
 		<View style={styles.container}>
 			<ErrorNotification error={localErorr} />
 			<KeyboardAvoidingView
-				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
 				style={styles.content}
 			>
 				<Text>School </Text>
-				<Image style={styles.logo} source={require('../assets/logo.png')} resizeMode="contain" />
+				<Image
+					style={styles.logo}
+					source={require("../assets/logo.png")}
+					resizeMode="contain"
+				/>
 				<View style={styles.form}>
 					<View
 						style={{
 							...styles.inputs,
-							flexDirection: orientation === Orientation.PORTRAIT_UP ? 'column' : 'row',
+							flexDirection:
+								orientation === Orientation.PORTRAIT_UP ? "column" : "row",
 						}}
 					>
 						<Input
 							style={{
 								width:
 									orientation === Orientation.PORTRAIT_UP
-										? 'auto'
-										: Dimensions.get('window').width / 2 - 16 - 48,
+										? "auto"
+										: Dimensions.get("window").width / 2 - 16 - 48,
 							}}
 							placeholder="Email"
 							onChangeText={setEmail}
@@ -86,8 +97,8 @@ export default function Login() {
 							style={{
 								width:
 									orientation === Orientation.PORTRAIT_UP
-										? 'auto'
-										: Dimensions.get('window').width / 2 - 16 - 48,
+										? "auto"
+										: Dimensions.get("window").width / 2 - 16 - 48,
 							}}
 							isPassword
 							placeholder="Пароль"
@@ -96,7 +107,7 @@ export default function Login() {
 					</View>
 					<Button text="Войти" onPress={submit} isloading={isLoading} />
 				</View>
-				<CustomLink href={'/restorej'} text={'Восстановить пароль'} />
+				<CustomLink href={"/restorej"} text={"Восстановить пароль"} />
 			</KeyboardAvoidingView>
 		</View>
 	);
@@ -105,16 +116,16 @@ export default function Login() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
+		justifyContent: "center",
 		padding: 55,
 		backgroundColor: Colors.black,
 	},
 	content: {
-		alignItems: 'center',
+		alignItems: "center",
 		gap: Gaps.g50,
 	},
 	form: {
-		alignSelf: 'stretch',
+		alignSelf: "stretch",
 		gap: Gaps.g16,
 	},
 	inputs: {
